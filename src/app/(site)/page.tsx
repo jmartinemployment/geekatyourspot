fc91f2202ef6c47ce3de569f019111ad66ac47e0
Jeff Martin <jmartinemployment@gmail.com>
2026-08-13 12:00:31 -0400

    Add viewport-fit=cover to viewport meta tag

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -340,6 +340,7 @@ export const generateViewport = (): Viewport => ({
   initialScale: 1,
   maximumScale: 5,
   userScalable: true,
+  viewportFit: 'cover',
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: '#ffffff' },
     { media: '(prefers-color-scheme: dark)', color: '#023059' },
